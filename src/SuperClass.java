214d13deafea39bc44040d55d3a6609ed905deba
upravaD <daktah@icloud.com>
2023-01-13 17:38:35 +0400

    message3

--- src/SuperClass.java
+++ src/SuperClass.java
@@ -1,3 +1,25 @@
 public class SuperClass {
     int x = 0;
+    int y = 1;
+
+    public SuperClass(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
